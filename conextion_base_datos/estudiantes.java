476508b79f39c711a1a26fc195ee55b8edd8f049
javier6441112 <jsandovalp9@miumg.edu.gt>
2023-12-08 21:54:48 -0600

    ah10

--- conextion_base_datos/estudiantes.java
+++ conextion_base_datos/estudiantes.java
@@ -3,112 +3,75 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package conextion_base_datos;
-
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-
+import java.util.Scanner;
 /**
  *
  * @author user
  */
 public class estudiantes {
+    String nombre;
+    String apellido;
+    int edad;
+    double nota;
+    int id_estudiantes;
 
-
-
-
-    Connection con;
-    PreparedStatement ps;
-    ResultSet rs;
-    conexion acceso = new conexion();
-
-    public void listar() {
-
-        String sql = "select * from estudiante";
-
-        try {
-
-//           
-            //Inicializa la base de datos
-            con = acceso.Conectar();//accede a  la base de datos
-            ps = con.prepareStatement(sql);// ejecuta la consulta
-            rs = ps.executeQuery();// guarda el resultado
-            while (rs.next()) {
-                System.out.println(rs.getInt(1) + " - " + rs.getString(2)+ " - " + rs.getString(3)
-                + " - " + rs.getString(4)+ " - " + rs.getString(5));
-            }
-        } catch (Exception e) {
-            System.out.println(e);
-        }
-
+    public estudiantes(String nombre, String apellido, int edad, double nota) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.edad = edad;
+        this.nota = nota;
     }
 
-    public void crear(ingresar datos) {
-
-        String sql = "insert into estudiante(nombre,apellido,edad,nota) values (?,?,?,?);";
-        try {
-            con = acceso.Conectar();
-            ps = con.prepareStatement(sql);
-            ps.setString(1, datos.nombre);
-            ps.setString(2, datos.apellido);
-            ps.setInt(3, datos.edad);
-             ps.setDouble(4, datos.nota);
-
-            ps.executeUpdate();
-
-        } catch (Exception e) {
-            System.out.println(e);
-        }
+    public String getNombre() {
+        return nombre;
     }
 
-    public void modificar(ingresar mod) {
-
-        String sql = "update estudiante set nombre=?, apellido=?, edad=?, nota=? where id_estudiantes =?   ;";
-        try {
-            con = acceso.Conectar();
-            ps = con.prepareStatement(sql);
-          
-            ps.setString(1, mod.nombre);
-            ps.setString(2, mod.apellido);
-            ps.setInt(3, mod.edad);
-            ps.setDouble(4, mod.nota);
-            ps.setInt(5, mod.id_estudiantes);
-            ps.executeUpdate();
-        } catch (Exception e) {
-        }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
     }
 
-    public void eliminar(ingresar elim) {
-        String sql = "delete from estudiante where id_estudiantes=?";
-        try {
-            con = acceso.Conectar();
-            ps = con.prepareStatement(sql);
-            ps.setInt(1, elim.id_estudiantes);
-            ps.executeUpdate();
-        } catch (Exception e) {
-        }
+    public String getApellido() {
+        return apellido;
     }
 
-  
-
-    public static void main(String[] args) {
-        estudiantes es = new estudiantes();
-        
-    
-        
-
-        es.listar();
-        es.crear(new ingresar("carlos","enrique",32,70));
-        System.out.println("+++++++++++++++++++++++++++");
-        es.listar();
-        System.out.println("+++++++++++++++++++++++++++");
-        es.modificar(new ingresar("azu","vasquez",45,67,2));
-        es.listar();
-        System.out.println("+++++++++++++++++++++++++++");
-        es.eliminar(new ingresar(5));
-         es.listar();
-
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
     }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public double getNota() {
+        return nota;
+    }
+
+    public void setNota(double nota) {
+        this.nota = nota;
+    }
+
+    public  estudiantes (String nombre, String apellido, int edad, double nota, int id_estudiantes) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.edad = edad;
+        this.nota = nota;
+        this.id_estudiantes = id_estudiantes;
+    }
+
+    public estudiantes(int id_estudiantes) {
+        this.id_estudiantes = id_estudiantes;
+    }
+
+    public int getId_estudiante() {
+        return id_estudiantes;
+    }
+
+    public void setId_estudiante(int id_estudiante) {
+        this.id_estudiantes = id_estudiante;
+    }
+
 }
-
-
---
+++ conextion_base_datos/menu.java
@@ -0,0 +1,88 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package conextion_base_datos;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author user
+ */
+public class menu {
+ String nombre_menu;
+    String apellido_menu;
+    int edad_menu;
+    double nota_menu;
+    int id_estudiantes_menu;
+    public void menu() {
+        
+         while (true) {
+           estudiantesDAO es = new estudiantesDAO();
+        Scanner sp = new Scanner(System.in);
+             System.out.println("");
+        System.out.println("++++++++++++++++++++++++++++++++++");
+        System.out.println("ver base de datos                1");
+        System.out.println("crear nuevo usuario              2");
+        System.out.println("modificar datos                  3");
+        System.out.println("eliminar usuario                 4");
+        System.out.println("salir                            5");
+        System.out.println("++++++++++++++++++++++++++++++++++");
+
+        switch (sp.nextInt()) {
+            case 1:
+                
+               es.listar();
+
+                break;
+                
+            case 2:
+                System.out.println("ingrese el nombre: ");
+                nombre_menu = sp.next();
+               System.out.println("ingrese el apellido: " );
+                apellido_menu = sp.next();
+               System.out.println("ingrese la edad: ");
+                edad_menu = sp.nextInt();
+               System.out.println("ingrese la nota: ");
+                nota_menu = sp.nextDouble();
+                es.crear(new estudiantes(nombre_menu,apellido_menu,edad_menu,nota_menu));
+                es.listar();
+
+                break;
+            case 3:
+                System.out.println("ingrese el id del estudiante a modificar: ");
+                id_estudiantes_menu = sp.nextInt();
+                  System.out.println("ingrese el nombre: ");
+                nombre_menu = sp.next();
+               System.out.println("ingrese el apellido: " );
+                apellido_menu = sp.next();
+               System.out.println("ingrese la edad: ");
+                edad_menu = sp.nextInt();
+               System.out.println("ingrese la nota: ");
+                nota_menu = sp.nextDouble();
+                es.modificar(new estudiantes(nombre_menu,apellido_menu,edad_menu,nota_menu));
+                es.listar();
+
+
+                break;
+            case 4:
+                System.out.println("ingrese el id del estudiante a eliminar: ");
+                id_estudiantes_menu = sp.nextInt();
+                es.eliminar(new estudiantes(id_estudiantes_menu));
+                es.listar();
+
+                break;
+                case 5:
+                    System.exit(0);
+                    break;
+
+        }
+         }
+
+    }
+    public static void main(String[] args) {
+        menu ejecutable = new menu();
+        ejecutable.menu();
+    }
+}
